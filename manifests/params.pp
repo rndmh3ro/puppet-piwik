7a81694b7784330d6c6a633287f6becfd9f69d54
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2017-02-08 14:05:06 +0100

    update piwik config template

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,17 @@
 class piwik::params {
-  $web_server = 'apache'
-  $version    = 'latest'
-  $download_baseurl = 'http://builds.piwik.org/'
+  $web_server                     = 'apache'
+  $version                        = 'latest'
+  $download_baseurl               = 'http://builds.piwik.org/'
+  $piwik_db_host                  = undef
+  $piwpiwik_db_username           = undef
+  $piwpiwik_db_password           = undef
+  $piwpiwik_db_dbname             = undef
+  $piwpiwik_db_port               = undef
+  $piwpiwik_rec_mail_addr         = undef
+  $piwpiwik_noreply_mail_addr     = undef
+  $piwpiwik_tracker_cookie_expire = undef
+  $piwpiwik_mail_host             = undef
+  $piwik_trusted_hosts            = []
 
   if $web_server {
     if $web_server == 'apache' {
